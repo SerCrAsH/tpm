e231e3a5a8dddbf11aabdbfab60911371c56cfe2
SerCrAsH <SerCrAsH@users.noreply.github.com>
2015-07-08 23:34:25 +0200

    [SerCrAsH] 0.0.2 + roadmap

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# tpm
-TamperMonkey Script
+# TamperMonkey Script para Forocoches
+TamperMonkey Script con diversas mejoras para [Forocoches](http://www.forocoches.com/).
+
+***
+## Versión : 
+
+### 0.0.2
+* Preview funcionalidades añadidas :
+  * Implementado sobre el listado de foros
+    * DarkTheme Forocoches FC 2.0. minificado 
+      * (by cowabunga, [ver perfil stylish](http://userstyles.org/styles/84703/forocoches-dark-style-2-0) )
+    * Estilo bootstrap
+    * Ocultación de adds
+    * Barra de búsqueda en cabecera
+    * Previsualización del contenido del hilo
+
+### 0.0.1 
+* Estado inicial ( arquitectura aplicada )
+
+***
+## Roadmap :
+### Target - 1.0.0
+- [ ] Aplicar script a toda la web de FC.
+- [ ] Panel de opciones
+  - [ ] Carga de selector de tema al posicionarse sobre la cabecera de la página (slideDown)
+  - [ ] Opción de ocultar hilos asociados a un personaje
+  - [ ] Creación de cookies asociadas al perfil del script
+- [ ] En wyswyg agregar botón para subir archivos a imgur desde local
+- [ ] Visualización de web.m para los principales servidores
+- [ ] Visualización de spotify web & soundcloud en los mensajes
+- [ ] Mejora del pop con información del autor (avatar + registro + mensajes ) + contenido inicial del mensaje
--- tpm-scripts/ForoCarros.user.js
+++ tpm-scripts/ForoCarros.user.js
@@ -1,20 +1,34 @@
 // ==UserScript==
 // @name         ForoCarros
-// @namespace    https://github.com/Sercrash-git
+// @namespace    https://github.com/SerCrAsH/tpm
 // @version      0.0.1
-// @description  Mejoras de forocoches
+// @description  Mejoras de forocoches ( Chrome versión web )
 // @author       SerCrAsH
-// @match        *.forocoches.com/foro/forumdisplay*
+// @match        *://www.forocoches.com/foro/forumdisplay*
 // @grant        none
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require      https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js
+// @resource     bootstrap_CSS https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css
+// @resource     darktheme_CSS https://raw.githubusercontent.com/SerCrAsH/tpm/master/tpm-scripts/vendor/css/Forocoches%20dark%20style%202.0.min.css
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // @noframes
 // @run-at       document-start
 // ==/UserScript==
 
-// Adds by Google
+/** Initial css reference **/
+// Bootstrap 
+var bootstrap_CSS = GM_getResourceText ("bootstrap_CSS");
+GM_addStyle (bootstrap_CSS);
+// Darktheme
+var darktheme_CSS = GM_getResourceText ("darktheme_CSS");
+GM_addStyle (darktheme_CSS);
+
+/** Remove adds **/
+// Adds
 jQuery("head").prepend('<style type="text/css">.cajasprin { display:none !important; }</style>');
 
+/*******************************************************************************************************/
 /** Script code **/
 // IIFE - Immediately Invoked Function Expression
 (function(scriptCode) {
@@ -25,13 +39,24 @@ jQuery("head").prepend('<style type="text/css">.cajasprin { display:none !import
     $(function() {
         var execution_start_time = +new Date();
         // Init
-
-        // Get thread's <tr>
+        /*******************************************************************************************************/
+        /** Vars **/
+        var $table_list = $('#threadslist');
         var $table_threads = $('#threadslist').find("[id^=threadbits]");
 
+        /*******************************************************************************************************/
+        /** Modules **/
+        // Retrieve search top bar
+        retrieve_search_top_bar();
+
+        // Table_List fix
+        fix_table($table_list);
+
         // Start popovers :
-        $table_threads.children().each(setPops);
-        
+        $table_threads.children().each(setPos);
+
+        /*******************************************************************************************************/
+
 
 
         /*******************************************************************************************************/
@@ -40,11 +65,15 @@ jQuery("head").prepend('<style type="text/css">.cajasprin { display:none !import
     });
     // Independencies of DOM ready status
 
-    /** Event mouseover on tr fields **/
-    function setPops () {
+    /** Funciones  **/
+    // Event mouseover on tr fields
+    var setPos  = function () {
         var $ttitle = $(this).find('[id^=thread_title]');
+        var content = $(this).find('[id^=td_threadtitle]').prop('title');
 
-        var content = '<img src="http://abload.de/img/coche6gkdq.png" style="width: 80px; display:inline;">'+$ttitle.text();
+        // Content parse
+        content = '<img src="http://abload.de/img/coche6gkdq.png" style="width: 80px; display:block; margin:auto; text-align: center;">'+ content;
+
         $ttitle.popover({
             trigger: 'hover',
             content: content ,
@@ -52,8 +81,25 @@ jQuery("head").prepend('<style type="text/css">.cajasprin { display:none !import
             placement: 'bottom',
             html : true 
         });
-    }
+    };
+
+    /*******************************************************************************************************/
+    /** Modules functions **/
+    // Retrieve search top bar
+    var retrieve_search_top_bar = function(){
+        var $td_search = $('tr form[name="busca"]').parent().clone();
+        $td_search.addClass("form-control");
+        $('#AutoNumber6').append( $td_search[0] );
+
+    };
+
+    // Fix table list
+    var fix_table = function($target_table){
+        $target_table.addClass("table table-striped table-condensed");
+    };
+
 
 }
 ));
 // End script
+/*******************************************************************************************************/
